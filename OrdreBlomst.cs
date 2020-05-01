df2f371771fb29b676e819a08ad338eae0665730
jakobln <62882745+jakobln@users.noreply.github.com>
2020-05-01 11:15:54 +0200

    Tilføjet OrdreBlomst klasse
    Inkl. properties Navn, Antal og Farve

---
+++ OrdreBlomst.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blomsterbutik
+{
+    public class OrdreBlomst
+    {
+        private string _navn;
+        private int _antal;
+        private string _farve;
+
+        public string Navn 
+        {
+            get { return _navn; }
+            set { _navn = value; } 
+        }
+
+        public int Antal 
+        {
+            get { return _antal; }
+            set { _antal = value; }
+        }
+        public string Farve 
+        {
+            get { return _farve; }
+            set { _farve = value; }
+        }
+    }
+}
