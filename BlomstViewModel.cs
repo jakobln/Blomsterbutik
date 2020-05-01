026af9fd50c4e68e6ac4e7b0c23dafd7e166e38a
jakobln <62882745+jakobln@users.noreply.github.com>
2020-05-01 11:25:01 +0200

    Tilføjet ViewModel klasse "BlomstViewModel"
    Inkl. ObservableCollection OC_Blomster

---
+++ BlomstViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blomsterbutik
+{
+    public class BlomstViewModel
+    {
+        public ObservableCollection<OrdreBlomst> OC_Blomster
+        {
+            get
+            {
+                return new ObservableCollection<OrdreBlomst>()
+                {
+                    new OrdreBlomst() { Navn = "Tulipan", Antal = 5, Farve = "Rød"},
+                    new OrdreBlomst() { Navn = "Tulipan", Antal = 3, Farve = "Grøn"}
+                };
+            }
+        }
+
+    }
+}
